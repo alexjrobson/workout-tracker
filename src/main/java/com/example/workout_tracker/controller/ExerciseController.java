bdc95548a280534bfbbf3264d59759a2417da0ec
alexjrobson <alex_j_robson@hotmail.com>
2025-08-07 10:49:17 +0800

    Added ExerciseRequst, added a POST createExercise.

--- src/main/java/com/example/workout_tracker/controller/ExerciseController.java
+++ src/main/java/com/example/workout_tracker/controller/ExerciseController.java
@@ -1,13 +1,19 @@
 package com.example.workout_tracker.controller;
 
+import com.example.workout_tracker.dto.ExerciseRequest;
 import com.example.workout_tracker.model.Exercise;
+import com.example.workout_tracker.model.Workout;
 import com.example.workout_tracker.repository.ExerciseRepository;
+import com.example.workout_tracker.repository.WorkoutRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -15,37 +21,39 @@ import java.util.List;
 public class ExerciseController {
 
     @Autowired
-    private ExerciseRepository repo;
+    private WorkoutRepository workoutRepository;
+    @Autowired
+    private ExerciseRepository exerciseRepository;
 
     @GetMapping
     public List<Exercise> getAllExercises(){
-        return repo.findAll();
+        return exerciseRepository.findAll();
     }
     @GetMapping
     public List<Exercise> getExercises(@RequestParam(required = false) Long workoutId){
         if(workoutId != null){
-            return repo.findbyWorkoutId(workoutId);
+            return exerciseRepository.findbyWorkoutId(workoutId);
         }
         else{
-            return repo.findAll();
+            return exerciseRepository.findAll();
         }
     }
 
     @PostMapping
     public ResponseEntity<Exercise> createExercise(@RequestBody Exercise exercise) {
-        Exercise saved = repo.save(exercise);
+        Exercise saved = exerciseRepository.save(exercise);
         return new ResponseEntity<>(saved, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Exercise> updateExercise(@PathVariable Long id, @RequestBody Exercise updatedExercise) {
-        return repo.findById(id)
+        return exerciseRepository.findById(id)
                 .map(exercise -> {
                     exercise.setName(updatedExercise.getName());
                     exercise.setReps(updatedExercise.getSets());
                     exercise.setSets(updatedExercise.getSets());
                     exercise.setWorkout(updatedExercise.getWorkout()); // Optional
-                    Exercise saved = repo.save(exercise);
+                    Exercise saved = exerciseRepository.save(exercise);
                     return ResponseEntity.ok(saved);
                 })
                 .orElse(ResponseEntity.notFound().build());
@@ -53,10 +61,33 @@ public class ExerciseController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteExercise(@PathVariable Long id){
-        if(!repo.existsById(id)){
+        if(!exerciseRepository.existsById(id)){
             return ResponseEntity.notFound().build();
         }
-        repo.deleteById(id);
+        exerciseRepository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PostMapping
+    public ResponseEntity<Exercise> createExercise(@RequestBody ExerciseRequest request) {
+
+        Optional<Workout> workoutOptional = workoutRepository.findById(request.getWorkoutId()); // might contain a Workout, or might be empty
+
+        if (workoutOptional.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Workout not found");
+        }
+            Workout workout = workoutOptional.get();
+            Exercise exercise = new Exercise(
+                    request.getName(),
+                    request.getReps(),
+                    request.getSets(),
+                    workout);
+
+           Exercise savedExercise = exerciseRepository.save(exercise);
+
+           return ResponseEntity.status(HttpStatus.CREATED).body(savedExercise);
+
+
+}
+
 }
---
+++ src/main/java/com/example/workout_tracker/dto/ExerciseRequest.java
@@ -0,0 +1,44 @@
+package com.example.workout_tracker.dto;
+
+public class ExerciseRequest {
+
+    private String name;
+
+    private int reps;
+
+    private int sets;
+
+    private Long workoutId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public void setReps(int reps) {
+        this.reps = reps;
+    }
+
+    public int getSets() {
+        return sets;
+    }
+
+    public void setSets(int sets) {
+        this.sets = sets;
+    }
+
+    public Long getWorkoutId() {
+        return workoutId;
+    }
+
+    public void setWorkoutId(Long workoutId) {
+        this.workoutId = workoutId;
+    }
+}
